316752ced59a7f593560aab57bdaeeb07184eee2
Devanshu M <devansm@microsoft.com>
2024-01-11 11:59:52 +0530

    add one more test

--- src/test/java/org/deliveroo/cronparser/ParseExpressionTests.java
+++ src/test/java/org/deliveroo/cronparser/ParseExpressionTests.java
@@ -19,4 +19,14 @@ public class ParseExpressionTests {
         assertEquals(exp.getFields()[3].getItems(), List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
         assertEquals(exp.getFields()[4].getItems(), List.of(1, 2, 3, 4, 5));
     }
+
+    @Test
+    public void Test2() {
+        CronExpression exp = CronExpression.Parse("5,10 0 2 * * ls");
+        assertEquals(exp.getFields()[0].getItems(), List.of(5, 10));
+        assertEquals(exp.getFields()[1].getItems(), List.of(0));
+        assertEquals(exp.getFields()[2].getItems(), List.of(2));
+        assertEquals(exp.getFields()[3].getItems(), List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+        assertEquals(exp.getFields()[4].getItems(), List.of(1, 2, 3, 4, 5, 6, 7));
+    }
 }
