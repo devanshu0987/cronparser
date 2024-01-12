8a355ae2e7984f58cd26485f6f65482ddf5b2e42
Devanshu M <devansm@microsoft.com>
2024-01-12 15:33:05 +0530

    main update

--- src/main/java/org/deliveroo/cronparser/Main.java
+++ src/main/java/org/deliveroo/cronparser/Main.java
@@ -4,8 +4,11 @@ import org.deliveroo.cronparser.model.CronExpression;
 
 public class Main {
     public static void main(String[] args) {
-
-        CronExpression exp = CronExpression.parse(args[0]);
-        System.out.println(exp.printParsedExpression());
+        if (args == null || args.length == 0) {
+            System.out.println("Invalid Input");
+        } else {
+            CronExpression exp = CronExpression.parse(args[0]);
+            System.out.println(exp.printParsedExpression());
+        }
     }
 }
